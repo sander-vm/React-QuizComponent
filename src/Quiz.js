011e8f7e3a504261b76c19a89f52dee4fd531612
Sander Van Mullem <Sander.VanMullem@infosupport.com>
2019-03-01 14:50:02 +0100

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,7 +21,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd && <QuizEnd /> }
-        {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1] } showNextQuestion={this.showNextQuestion.bind(this)}/>}
+        {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1] } showNextQuestionHandler ={this.showNextQuestion.bind(this)}/>}
       </div>)
     ;
   }
