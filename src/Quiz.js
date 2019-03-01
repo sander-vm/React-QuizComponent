1b3c629887e0a59d13db18c393bf3e3ed33b9b4b
Sander Van Mullem <Sander.VanMullem@infosupport.com>
2019-03-01 15:05:47 +0100

    module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,6 +16,10 @@ class Quiz extends Component {
     })
   }
 
+  handleResetClick(){
+    this.setState({quiz_position: 1});
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
